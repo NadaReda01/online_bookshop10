ed4060f4b44c37f606731e5a188c47fe032525d3
NadaReda01 <87943748+NadaReda01@users.noreply.github.com>
2022-01-20 00:34:53 +0200

    Add files via upload

---
+++ articles.php
@@ -0,0 +1,178 @@
+<?php
+require 'dbconnection.php';
+
+  $sql = "select articles.* , users.name from articles join users on articles.user_id = users.id";
+
+  $op  = mysqli_query($con,$sql); 
+
+?>
+
+
+
+<!DOCTYPE html>
+<!-- saved from url=(0014)about:internet -->
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="website that help people benefit their old books" />
+    <meta name="keywords" content="book ,reading ,exchangebook" />
+    <meta name="author" content="Mariam Essam" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+    <link rel="stylesheet" href="frontassets/css/style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="frontassets/css/all.css" />
+    <title>Exchange_books</title>
+</head>
+
+<body>
+    <div class="bg">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="#"><span>Reading_Exchange</span></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon">
+                        <i class="fa fa-navicon" style="color: #fff; font-size: 26px; margin: 0px"></i>
+                    </span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item ">
+                            <a class="nav-link" href="index.php">Home </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.html">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="categories.html">Categories</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Gallery.html">Gallery</a>
+                        </li>
+                        <li class="nav-item  active">
+                            <a class="nav-link " href="articles.php">Articles</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="addarticles.php">addarticles</a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link" href="login.php">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- end navbar -->
+        <!-- start header -->
+        <header>
+            <div class="container">
+                <div>
+                    <h1>Our Articles</h1>
+                    <h4>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
+                        veritatis in <br />tenetur doloremque, maiores doloribus officia
+                        iste. Dolores. .
+                    </h4>
+                    <a href="#">
+                        <div id="button">Learn More</div>
+                    </a>
+                </div>
+            </div>
+        </header>
+    </div>
+    <!-- end header -->
+    <!-- start ourblog -->
+    <section id="section_blog">
+        <div class="container">
+            <div class="row">
+
+
+
+
+            <?php 
+                    // Fetch data .... 
+                while($data = mysqli_fetch_assoc($op)){
+
+            ?>
+
+
+                <div class="col-lg-4 col-md-6 col-12">
+                    <div class="box">
+                        <img src=" ./uploads/<?php echo $data['image']; ?>" />
+                        <h1><?php echo $data['title'];?></h1>
+                        <pre><h3><?php echo $data['time'];?> by</h3> <h3 id="color"><?php echo $data['name'];?></h3></pre>
+                        <p>
+
+                        <?php echo $data['content'];?>
+                        
+                    </p>
+
+                    </div>
+                </div>
+                
+
+                <?php } ?>
+
+
+            </div>
+        </div>
+    </section>
+    <!-- end ourblog -->
+    <!-- start footer -->
+    <footer>
+        <div class="container text-center">
+            <div id="first">
+                <h1>Reading_Exchange</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="../index.html">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="categories.html">categories</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </nav>
+            <ul id="ulicon2">
+                <li>
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                </li>
+            </ul>
+            <hr />
+        </div>
+    </footer>
+    <!-- end footer -->
+    <!-- start siction loading   -->
+    <section class="loading-overlay">
+        <div class="lds-ring">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </section>
+    <!-- end section loading -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="frontassets/js/project.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
